6b76fc5acb92b8b3e311521bafdc5208ba56c8e7
blockowe <blockowe@atanasoff.cs.grinnell.edu>
2025-02-18 20:26:39 -0600

    Created all test cases for SimpleStringBuffer.

--- src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
+++ src/test/java/edu/grinnell/csc207/texteditor/SimpleStringBufferTests.java
@@ -1,5 +1,72 @@
 package edu.grinnell.csc207.texteditor;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import net.jqwik.api.*;
+import net.jqwik.api.constraints.*;
+
 public class SimpleStringBufferTests {
-    /** TODO: fill me in with unit and property tests! */
+   
+    @Test
+    public void testAddEmptyDeleteBeginning() {
+        SimpleStringBuffer buffer = new SimpleStringBuffer();
+        buffer.insert('a');
+        assertEquals(1 , buffer.getCursorPosition());
+        assertEquals("a" , buffer.toString());
+        buffer.delete();
+        assertEquals("" , buffer.toString());
+    }
+    
+    @Test 
+    public void testAddEndDeleteEnd() {
+        SimpleStringBuffer buffer = new SimpleStringBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        assertEquals(2 , buffer.getCursorPosition());
+        buffer.insert('c');
+        assertEquals("abc" , buffer.toString());
+        buffer.delete();
+        assertEquals("ab" , buffer.toString());
+    }
+    
+    @Test
+    public void testMoveCursorAddMiddleDeleteMiddle() {
+        SimpleStringBuffer buffer = new SimpleStringBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.insert('c');
+        assertEquals(3 , buffer.getCursorPosition());
+        buffer.moveLeft();
+        buffer.moveLeft();
+        assertEquals(1 , buffer.getCursorPosition());
+        buffer.moveRight();
+        assertEquals(2 , buffer.getCursorPosition());
+        buffer.delete();
+        assertEquals("ac" , buffer.toString());
+    }
+    
+    @Test
+    public void testGetChar() {
+        SimpleStringBuffer buffer = new SimpleStringBuffer();
+        buffer.insert('a');
+        buffer.insert('b');
+        buffer.insert('c');
+        assertEquals('b' , buffer.getChar(1));
+    }
+    
+    @Property
+    public boolean testCursorMovement(@ForAll @IntRange(min = 0, max = 1000) int sz) {
+        SimpleStringBuffer buffer = new SimpleStringBuffer();
+        for (int i = 0; i < sz; i++) {
+            buffer.insert('a');
+	}
+
+		return buffer.getCursorPosition() == sz;
+
+	} 
+
+        
+
+    
 }
