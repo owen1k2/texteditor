ddd0a0beb2026925c4f109492a67cf306cead2f0
blockowe <blockowe@atanasoff.cs.grinnell.edu>
2025-02-20 10:41:40 -0600

    Style Updates of SimpleStringBuffer.

--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -4,51 +4,56 @@ package edu.grinnell.csc207.texteditor;
  * A naive implementation of a text buffer using a <code>String</code>.
  */
 public class SimpleStringBuffer {
-    
+
     String s;
+    
     int cursor;
-    
-    
+
     /**
      * Creates a new and empty SimpleStringBuffer.
      */
-    public void SimpleStringBuffer() {
+    public void simpleStringBuffer() {
         cursor = 0;
         s = null;
     }
-    
+
     /**
-     * Inserts the given character at the cursors given position and moves the 
+     * Inserts the given character at the cursors given position and moves the
      * cursor forward one space.
+     *
      * @param ch : The character to be inserted
      */
     public void insert(char ch) {
-        if(s == null) {
+        if (s == null) {
             s = ch + "";
-        } else if(cursor == s.length()) {
+        } else if (cursor == s.length()) {
             s = s + ch;
-        } else if(cursor == 0) {
+        } else if (cursor == 0) {
             s = ch + s;
         } else {
-        s = s.substring(0 , cursor) + ch + s.substring(cursor , s.length());
+            s = s.substring(0, cursor) + ch + s.substring(cursor, s.length());
         }
         cursor++;
     }
 
-    
+    /**
+     * Deletes the character at the cursors given position and moves the cursor
+     * back one space if not already there.
+     */
     public void delete() {
-      if (cursor == s.length()) {
+        if (cursor == s.length()) {
             s = s.substring(0, s.length() - 1);
             cursor--;
         } else if (cursor == 0) {
-            s = s.substring(1 , s.length());
+            s = s.substring(1, s.length());
         } else {
-            s = s.substring(0 , cursor - 1) + s.substring(cursor , s.length());
+            s = s.substring(0, cursor - 1) + s.substring(cursor, s.length());
         }
     }
 
     /**
      * Returns the current position of the cursor.
+     *
      * @return int : The position of the cursor.
      */
     public int getCursorPosition() {
@@ -59,7 +64,7 @@ public class SimpleStringBuffer {
      * Moves the cursor one position to the left.
      */
     public void moveLeft() {
-        if(cursor > 0) {
+        if (cursor > 0) {
             cursor--;
         }
     }
@@ -68,33 +73,36 @@ public class SimpleStringBuffer {
      * Moves the cursor one position to the right.
      */
     public void moveRight() {
-        if(cursor < s.length()) {
+        if (cursor < s.length()) {
             cursor++;
         }
     }
 
     /**
      * Returns the size of the SimpleStringBuffer
+     *
      * @return int : the size of SimpleStringBuffer.
      */
     public int getSize() {
         return s.length();
     }
 
-   /**
+    /**
      * Returns the char from SimpleStringBuffer at index i
+     *
      * @param i : int, the index of which the char is returned from
      * @return char : The char in the string SimpleStringBuffer at index i.
      */
     public char getChar(int i) {
-        if(i > s.length()) {
-             throw new IndexOutOfBoundsException();
+        if (i > s.length()) {
+            throw new IndexOutOfBoundsException();
         }
         return s.charAt(i);
     }
 
     /**
      * Returns the contents of SimpleStringBuffer
+     *
      * @return String : Returns the string that is the SimpleStringBuffer
      */
     @Override
